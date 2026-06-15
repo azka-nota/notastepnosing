1477cf527b6ca303f3ae075b0211261c052fb579
azka-nota <azkacloud01@gmail.com>
2026-06-15 19:10:57 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'azka-nota-v1';
+const assets = [
+  './',
+  './index.html',
+  './logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
